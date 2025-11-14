acf9cfe9929899b2f0c1de5bf9c219494992f6d4
Ubada-S <ubadasiddiqicoc@gmail.com>
2025-11-14 21:20:38 +0530

    hmphh

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
         <nav className=" px-3.5 flex-center-between w-full max-w-7xl mx-auto">
           <div className="flex-center gap-x-3 z-[999] relative">
             <img src={Logo} alt="" className="size-8" />
-            <h3 className="text-lg font-semibold">Framer</h3>
+            <h3 className="text-lg font-semibold">Google</h3>
           </div>
 
           <ul className="gap-x-1 lg:flex-center hidden">
